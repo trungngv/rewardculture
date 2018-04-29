639d93166aa070e94e5491c28aad150cc49357dc
Trung Nguyen <trung.ngvan@gmail.com>
2018-04-29 22:57:33 +1000

    Update book activity to handle review and like action, MVP complete!

--- app/src/main/java/com/rewardculture/view/BookActivity.java
+++ app/src/main/java/com/rewardculture/view/BookActivity.java
@@ -1,12 +1,13 @@
 package com.rewardculture.view;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,18 +18,21 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.like.LikeButton;
+import com.like.OnLikeListener;
 import com.rewardculture.R;
 import com.rewardculture.database.FirebaseDatabaseHelper;
 import com.rewardculture.misc.Constants;
 import com.rewardculture.misc.Utils;
 import com.rewardculture.model.Book;
+import com.rewardculture.model.PostedBySnippet;
 import com.rewardculture.model.Review;
-import com.rewardculture.database.LocalDatabase;
 import com.rewardculture.ost.OstEconomy;
 import com.rewardculture.ost.TokenEconomy;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class BookActivity extends AppCompatActivity {
 
@@ -51,6 +55,8 @@ public class BookActivity extends AppCompatActivity {
 
     Book book;
     DatabaseReference bookRef;
+    FirebaseListAdapter<Review> reviewsAdapter;
+    int lastPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +68,7 @@ public class BookActivity extends AppCompatActivity {
         String bookId = getIntent().getStringExtra(Constants.INTENT_BOOK);
         dbHelper = FirebaseDatabaseHelper.getInstance();
         bookRef = dbHelper.getBook(bookId);
-        bookRef.addValueEventListener(new ValueEventListener() {
+        bookRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Log.d(TAG, dataSnapshot.getValue().toString());
@@ -75,56 +81,94 @@ public class BookActivity extends AppCompatActivity {
 
             }
         });
+    }
 
-        // set up write review functionality
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String txtReview = inputReview.getText().toString();
-                if (!txtReview.isEmpty()) {
-                    // TODO use logged in user
-                    String userId = "testUserId";
-                    Review review = new Review(txtReview, userId, 0, 0);
-                    dbHelper.addReview(bookRef, review, new DatabaseReference.CompletionListener() {
-                        @Override
-                        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                            Utils.showToastAndLog(BookActivity.this,
-                                    "review pushed" + databaseReference.getKey(), TAG);
-                            try {
-                                String response = economy.executeReviewTransaction(dbHelper.getTestUuid());
-                                Log.d(TAG, "review transaction response: " + response);
-                            } catch (Exception e) {
-                                Log.e(TAG, "review transaction failed");
-                                Log.e(TAG, e.getMessage());
-                            }
-                        }
-                    });
-                    inputReview.getText().clear();
+    @OnClick(R.id.btn_review)
+    public void onSendReviewClick(View v) {
+        String txtReview = inputReview.getText().toString();
+        if (!txtReview.isEmpty()) {
+            // TODO use logged in user
+            String userId = "unknown";
+            Review review = new Review(txtReview, new PostedBySnippet(userId, "Trung Nguyen"));
+            // write review to database and execute ost transaction
+            dbHelper.addReview(bookRef, review, new DatabaseReference.CompletionListener() {
+                @Override
+                public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                    Utils.showToastAndLog(BookActivity.this,
+                            "review pushed" + databaseReference.getKey(), TAG);
+                    String response = economy.executeReviewTransaction(dbHelper.getTestUuid());
+                    Log.d(TAG, "review transaction response: " + response);
                 }
-            }
-        });
+            });
+            // reset state of input review
+            inputReview.getText().clear();
+        }
     }
 
     void updateUI() {
         bookTitle.setText(book.getTitle());
-        listView.setAdapter(createListAdapter(dbHelper.getReviews(bookRef)));
+        reviewsAdapter = createListAdapter(dbHelper.getReviews(bookRef));
+        listView.setAdapter(reviewsAdapter);
     }
 
-    private ListAdapter createListAdapter(Query query) {
+    private FirebaseListAdapter<Review> createListAdapter(Query query) {
         FirebaseListOptions options = new FirebaseListOptions.Builder<Review>()
                 .setQuery(query, Review.class)
                 .setLifecycleOwner(this)
                 .setLayout(R.layout.cardview_review)
                 .build();
 
-        FirebaseListAdapter adapter = new FirebaseListAdapter<Review>(options) {
-            @Override
-            protected void populateView(View v, Review model, int position) {
-                ((TextView) v.findViewById(R.id.cv_review_text)).setText(model.getText());
-            }
-        };
-
+        FirebaseListAdapter adapter = new ReviewsListAdapter(options);
         return adapter;
     }
 
+    class ReviewsListAdapter extends FirebaseListAdapter<Review> {
+
+        public ReviewsListAdapter(@NonNull FirebaseListOptions<Review> options) {
+            super(options);
+        }
+
+        @Override
+        protected void populateView(View v, Review model, final int position) {
+            // TODO use logged in user
+            String posterName = model.getPosterName();
+            if (posterName == null) {
+                posterName = "Anonymous user";
+            }
+            ((TextView) v.findViewById(R.id.cv_posted_by)).setText(posterName);
+            ((TextView) v.findViewById(R.id.cv_review_text)).setText(model.getText());
+            ((TextView) v.findViewById(R.id.txt_likes)).setText(
+                    String.valueOf(model.getNumberOfLikes()));
+            LikeButton btn = v.findViewById(R.id.btn_like);
+            btn.setLiked(model.likedByUser(dbHelper.getTestUuid()));
+            btn.setOnLikeListener(new OnLikeListener() {
+                @Override
+                public void liked(LikeButton likeButton) {
+                    String response = economy.executeLikeTransaction(dbHelper.getTestUuid());
+                    Log.d(TAG, "like transaction response" + response);
+                    dbHelper.likeReview(dbHelper.getTestUuid(), reviewsAdapter.getRef(position));
+                    lastPosition = position;
+                }
+
+                @Override
+                public void unLiked(LikeButton likeButton) {
+                    dbHelper.unlikeReview(dbHelper.getTestUuid(), reviewsAdapter.getRef(position));
+                    lastPosition = position;
+                }
+            });
+        }
+
+        @Override
+        public void onDataChanged() {
+            super.onDataChanged();
+            if (lastPosition == -1) {
+                listView.smoothScrollToPosition(reviewsAdapter.getCount());
+            } else {
+                Log.d(TAG, "last position = " + lastPosition);
+                listView.smoothScrollToPosition(lastPosition);
+            }
+        }
+
+    }
 }
+
